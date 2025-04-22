9ed2ce00719bc02705b190a02660791acd2a6cff
nguyentuss <nttu.93tn@gmail.com>
2025-04-22 22:41:17 +0700

    Fixing error mvn clean package

--- be/src/test/java/com/example/hcmiuweb/HcmiuWebApplicationTests.java
+++ be/src/test/java/com/example/hcmiuweb/HcmiuWebApplicationTests.java
@@ -5,16 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=password",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-    "spring.jpa.hibernate.ddl-auto=create-drop",
-    "spring.security.user.name=testuser",
-    "spring.security.user.password=testpassword"
-})
 class HcmiuWebApplicationTests {
 
     @Test
