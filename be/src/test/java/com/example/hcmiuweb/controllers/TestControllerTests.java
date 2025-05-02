5481a4c1100016ea3add8ef9a577cd8364fc0056
nguyentuss <nttu.93tn@gmail.com>
2025-05-02 09:45:14 +0700

    Fix TestControllerTests by adding mock beans for JWT authentication

--- be/src/test/java/com/example/hcmiuweb/controllers/TestControllerTests.java
+++ be/src/test/java/com/example/hcmiuweb/controllers/TestControllerTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +12,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.example.hcmiuweb.HcmiuWebApplication;
 import com.example.hcmiuweb.controllers.TestController;
+import com.example.hcmiuweb.config.jwt.JwtUtils;
+import com.example.hcmiuweb.services.UserDetailsServiceImpl;
 
 @WebMvcTest(TestController.class) // Specify the controller class explicitly
 @ContextConfiguration(classes = HcmiuWebApplication.class)
@@ -18,6 +21,12 @@ public class TestControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @MockBean
+    private JwtUtils jwtUtils;
+    
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
 
     @Test
     @WithMockUser // This annotation mocks a logged-in user for the test
